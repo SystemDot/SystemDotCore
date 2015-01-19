44351c91a3663ccdd1e3e60c950f9baec428d71b
sammosampson <sammosampson@gmail.com>
2015-01-19 20:14:27 +0000

    Added env bootstrapping

---
+++ New/Environment/SystemDot.Environment.Windows/BuilderConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using SystemDot.Bootstrapping;
+
+namespace SystemDot.Environment
+{
+    public static class BuilderConfigurationExtensions
+    {
+        public static BootstrapBuilderConfiguration UseEnvironment(this BootstrapBuilderConfiguration configuration)
+        {
+            return configuration.RegisterBuildAction(c => c.RegisterInstance<ILocalMachine, LocalMachine>());
+        }
+    }
+}
